1530723386ec40655a350b4977056410d9cf01fb
sarabara-ux-ui <75815319+sarabara-ux-ui@users.noreply.github.com>
2021-02-03 12:47:16 -1000

    JS added
    Jquery added

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log ("woohoo, designers learn to code!")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
+
